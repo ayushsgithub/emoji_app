3c5a9ddc250aa49285740ffea44ecb6ecdadc8b7
Ayush Kumar <ayushchaurasiya78@gmail.com>
2024-09-25 17:13:40 +0530

    sign out functionality added

--- app/src/main/java/com/example/emoji/MainActivity.kt
+++ app/src/main/java/com/example/emoji/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.emoji
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var btSignOut: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,6 +20,15 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btSignOut = findViewById(R.id.btSignOut)
+
+        btSignOut.setOnClickListener(){
+
+            Firebase.auth.signOut()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
 
 
